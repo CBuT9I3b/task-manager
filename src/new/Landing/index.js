ec110650d072b8cfad791b0bdafddaec10fc87b4
Mike <mihail.xan@yandex.ua>
2019-04-18 15:50:45 +0300

    update topnav, sidenav and landing

--- src/new/Landing/index.js
+++ src/new/Landing/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Landing = () => (
-  <h5>Landing</h5>
+  <div>
+    <h5>Home</h5>
+    <p>
+      Welcome to the little simple task manager.
+      To start using the service you need to <Link to='/sign_in'>Sign In</Link>.
+    </p>
+  </div>
 );
 
 export default Landing
