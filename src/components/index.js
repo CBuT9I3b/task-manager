7dffa1c848e7493d127789511eca80a627e80637
Mike <mihail.xan@yandex.ua>
2019-06-04 16:13:00 +0300

    added preloader

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,16 @@
-export { default as SignIn } from './SignIn'
-export { default as SignUp } from './SignUp'
-export { default as SignOut } from './SignOut'
-
-export { default as SideNav } from './SideNav'
-export { default as UserBar } from './UserBar'
-export { default as Todos } from './Todos'
-export { default as TopNav } from './TopNav'
-export { default as Header } from './Header'
+export { default as Preloader } from './Preloader'
 
 export { default as Landing } from './Landing'
+export { default as SignIn } from './SignIn'
+export { default as SignUp } from './SignUp'
 export { default as Tasks } from './Tasks'
 export { default as Main } from './Main'
+
+export { default as UserBar } from './UserBar'
+export { default as SignOut } from './SignOut'
+export { default as Todos } from './Todos'
+export { default as SideNav } from './SideNav'
+export { default as TopNav } from './TopNav'
+export { default as Header } from './Header'
 
 export { default as App } from './App'
