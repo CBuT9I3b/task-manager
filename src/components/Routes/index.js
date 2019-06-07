edd5df27d8a5f108d0f7839e7a44d1811fbc05af
Mike <mihail.xan@yandex.ua>
2019-06-07 09:20:11 +0300

    added lazy loading routes

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 import { withUser } from '../../hocs'
 
-import { PublicHomePage, HomePage, SignInPage, SignUpPage } from '..'
+import { Preloader } from '..'
+
+const PublicHomePage = lazy(() => import('../PublicHomePage'));
+const HomePage = lazy(() => import('../HomePage'));
+const SignInPage = lazy(() => import('../SignInPage'));
+const SignUpPage = lazy(() => import('../SignUpPage'));
 
 const Routes = ({ user }) => (
-  <Switch>
-    <Route exact path='/' render={() => (
-      user ? <Redirect to='/manager' /> : <PublicHomePage />
-    )} />
-    <Route path='/manager' render={() => (
-      user ? <HomePage /> : <Redirect to='/' />
-    )} />
-    <Route path='/sign_in' render={() => (
-      user ? <Redirect to='/' /> : <SignInPage />
-    )} />
-    <Route path='/sign_up' render={() => (
-      user ? <Redirect to='/' /> : <SignUpPage />
-    )} />
-  </Switch>
+  <Suspense fallback={<Preloader />}>
+    <Switch>
+      <Route exact path='/' render={() => (
+        user ? <Redirect to='/manager' /> : <PublicHomePage />
+      )} />
+      <Route path='/manager' render={() => (
+        user ? <HomePage /> : <Redirect to='/' />
+      )} />
+      <Route path='/sign_in' render={() => (
+        user ? <Redirect to='/' /> : <SignInPage />
+      )} />
+      <Route path='/sign_up' render={() => (
+        user ? <Redirect to='/' /> : <SignUpPage />
+      )} />
+    </Switch>
+  </Suspense>
 );
 
 export default withUser(Routes)
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
 export { default as Preloader } from './Preloader'
 
-export { default as PublicHomePage } from './PublicHomePage'
-export { default as SignInPage } from './SignInPage'
-export { default as SignUpPage } from './SignUpPage'
+// export { default as PublicHomePage } from './PublicHomePage'
+// export { default as SignInPage } from './SignInPage'
+// export { default as SignUpPage } from './SignUpPage'
 export { default as Tasks } from './Tasks'
-export { default as HomePage } from './HomePage'
+// export { default as HomePage } from './HomePage'
 export { default as Routes } from './Routes'
 export { default as Main } from './Main'
 
